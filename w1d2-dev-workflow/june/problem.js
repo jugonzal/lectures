5e99620e4fd6f3e00efd9ad83838396a027e5089
Juan <juan@videogami.co>
2017-05-30 09:26:51 -0400

    Stated the problem and pseudo-code

---
+++ w1d2-dev-workflow/june/problem.js
@@ -0,0 +1,25 @@
+// Write a node program that takes in an unlimited number 
+// of command line arguments, goes through each and 
+// prints out the sum of them. 
+// If any argument is not a whole number, skip it. 
+// Do support negative numbers though.
+
+
+
+// Read input parameters from command line (process.argv) 
+
+// count them ( .length ) -> numberOfArguments
+
+// Assume Total equals zero
+
+// Given the full list of such parameters... (numberOfArguments)
+
+  // if the current parameter is NOT a number
+
+    // discard
+
+  // else
+
+    // add them up -> Total
+
+// Display the Total
