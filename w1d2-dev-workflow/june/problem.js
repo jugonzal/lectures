b5ef4993bb92e8b3d97f0978b6cf6673044d0ecf
Juan <juan@videogami.co>
2017-05-30 09:53:19 -0400

    Figure out my test cases... almost there

--- w1d2-dev-workflow/june/problem.js
+++ w1d2-dev-workflow/june/problem.js
@@ -8,11 +8,24 @@
 
 // Read input parameters from command line (process.argv) 
 
+var parameters = process.argv;
+
 // count them ( .length ) -> numberOfArguments
+// var numberOfArguments = parameters.length - 2;
+// console.log("Count of Arguments: ", numberOfArguments);
 
 // Assume Total equals zero
+var total = 0;
 
 // Given the full list of such parameters... (numberOfArguments)
+
+for (let i in parameters) {
+   console.log("Inside parameters: ", i, parameters[i]); 
+   console.log(" - using isNaN: ", isNaN(parameters[i]));
+   // console.log(" - divisible by 1: ", parameters[i]/!1);
+   console.log(" - using modulo (is it whole): ", parameters[i]%1 === 0);
+}
+
 
   // if the current parameter is NOT a number
 
