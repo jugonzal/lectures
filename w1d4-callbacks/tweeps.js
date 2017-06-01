cded9ce7dbcb865c16eacc1210822ce7aecc0d5f
Juan <juan@videogami.co>
2017-06-01 09:26:34 -0400

    Tweeps with only a couple of strategies to display

--- w1d4-callbacks/tweeps.js
+++ w1d4-callbacks/tweeps.js
@@ -1,55 +1,18 @@
-var tweeps = ['@nytimes','@lighthouselabs','@wired'];
-tweeps.push ('@globalculture');
-tweeps.push ('@starwars');
+var tweeps = ["@waxy", "@fzero", "@mozilla"]
 
-function padding(str) {
-  var numberOfSpaces = 15 - str.length;
-  return ' '.repeat(numberOfSpaces);
+function consoleDisplay (array) {
+  console.log("console.log ", array);  
 }
 
-// var displayStrategies = {
-//   nice: function (things) {
-//     for (var oneThing of things) {
-//       console.log("- ", oneThing);
-//     }
-//   },
-//   raw: function (things) {
-//     for (var oneThing of things) {
-//       console.log(oneThing);
-//     }
-//   },
-//   pretty: function (things) {
-//     for (var oneThing of things) {
-//       console.log("-> ", oneThing,padding(oneThing)," <-");
-//     }
-
-//   }
-
-// }
-
-function display(things, displayStrategy) {
-  for (var oneThing of things) {
-    displayStrategy(oneThing);
-  }  
-}
-
-var displayStrategies = {
-  nice: function (thing) {
-    console.log("- ", thing);
-  },
-  raw: function (thing) {
-    console.log(thing);
-  },
-  pretty: function (thing) {
-    console.log("-> ", thing,padding(thing)," <-");
+function nicerDisplay (array) {
+  console.log("nicer display");
+  for (let i of array) {
+    console.log("- ", i);
   }
-
 }
 
-display(tweeps, displayStrategies.pretty);
+consoleDisplay(tweeps);
 
-display(tweeps, function (oneThing) {
-  console.log('**',oneThing,'**');
-})
+tweeps.push("@wired");
 
-
+nicerDisplay(tweeps);
