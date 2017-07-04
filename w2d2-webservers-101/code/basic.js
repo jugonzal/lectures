b7603992249aebe583190ae89ce196fc8221b793
Juan <juan@videogami.co>
2017-07-04 10:47:12 -0400

    W2D2 lecture for July cohort

--- w2d2-webservers-101/code/basic.js
+++ w2d2-webservers-101/code/basic.js
@@ -1,29 +1,66 @@
-
-let http = require('http');
+// let http = require('http');
+const express = require('express');
+const app = express();
 
 // define app constants
 const PORT = 3000;
 
-// create a server with a responder function
-let server = http.createServer(function respondToRequests(request, response){
-        // respond to all requests in this function
-    console.log("Request: ", request.url)
-    if (request.url == '/') {
-      //HOME PAGE
-      response.end('<H1>Linked IN</H1><p>Hello from the other siiiiiiide!</p><p>Go to our <a href="/about">About</a> page or our <a href="/careers">Careers</a> page');
-    } else if (request.url == '/about') {
-      //ABOUT PAGE
-      response.end('<H1>Linked IN</H1><p>We founded this company many years ago</p>');
-    } else if (request.url == '/careers') {
-      // WE ARE HIRING !
-      response.end('<H1>Linked IN</H1><p>We want YOU! <img src="https://www.marketslant.com/sites/default/files/field/image/uncle-sam-we-want-you1.jpg"></p>');
-    } else {
-      response.statusCode = 404;
-      response.end('Not Found. Go.');
-    }
+app.set("view engine", "ejs");
+
+// read about how to use a public directory...
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.send(`<h1>Welcome to our Summer Events.</h1>
+        <br />Go to <a href='/toronto'>Toronto</a> 
+        or <a href='/montreal'>Montreal</a>`);
 });
 
-// start the server
-server.listen(PORT, function onServerStart(){
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
+app.get('/toronto', function (req, res) {
+  res.render('toronto');
+})
+
+app.get('/montreal', function(req, res) {
+  res.render('montreal');
+})
+
+app.get('/city/:someCity', function(req, res) {
+  console.log('New city: ', req.params.someCity);
+  res.send('Comming to this city soon...');
+})
+
+// create a server with a responder function
+// let server = http.createServer(function respondToRequests(request, response){
+//     // respond to all requests in this function
+//     console.log("Request: ", request.method, request.url)
+
+//     if (request.url === '/toronto') {
+//       response.statusCode = 200;
+//       response.setHeader('content-type', 'text/html');
+//       response.end(`Larry The Duck at Harbourfront<br />
+//         <img src='https://pbs.twimg.com/media/DDl6BewXkAAIiES.jpg'>`);
+//     } else if (request.url === '/montreal') {
+//       response.statusCode = 200;
+//       response.setHeader('content-type', 'text/html');
+//       response.end(`Jazz Festival<br />
+//         <img src='http://www.montrealjazzfest.com/Content/Images/static/open_graph.jpg'>`);
+//     } else if (request.url === '/') {
+//       response.statusCode = 200;
+//       response.end(`<h1>Welcome to our Summer Events.</h1>
+//         <br />Go to <a href='/toronto'>Toronto</a> 
+//         or <a href='/montreal'>Montreal</a>`);
+//     } else {
+//       response.statusCode = 404;
+//       response.end(`Don't know anything about that`);
+//     }
+// });
+
+// // start the server
+// server.listen(PORT, function onServerStart(){
+//     console.log("Server listening on: http://localhost:%s", PORT);
+// });
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port 3000!')
+})
