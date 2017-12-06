8c48982049930ff277f5cec218b11f4e8f57f4eb
Juan <juan@videogami.co>
2017-12-06 11:05:44 -0500

    New Music Tracks example

--- w2d3-crud-with-express/initial/app.js
+++ w2d3-crud-with-express/initial/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 const morgan = require('morgan');
 
 // Initialize express
@@ -17,18 +18,83 @@ app.set("view engine", "ejs");
 // to use the bodyParser middleware to convert those types of data
 // into JS objects inside our functions.
 app.use(bodyParser.urlencoded({extended: false})); // forms
-app.use(bodyParser.json()); // JSON
+// app.use(bodyParser.json()); // JSON
+
+app.use(methodOverride('_method'))
 
 // I'm also adding a logging middleware so we can see what's going on
 // with our server. More info: https://github.com/expressjs/morgan
 app.use(morgan('dev'));
 
+let DB = [{ 
+  number: 1,
+  name: 'Mr Blue Sky',
+  artist: 'Electric Light Orchestra',
+  year: 1972
+}, {
+  number: 2,
+  name: 'Black Sabbath',
+  artist: 'Black Sabbath',
+  year: 1978  
+}]
+
+function create (track) {
+  DB.push(track)
+}
+
+function read (criteria, value) {
+  for (let i = 0; i< DB.length; i++) {
+    if (DB[i][criteria] == value) {
+      return DB[i]
+    } 
+  }
+}
+
+create({
+  number: 3,
+  name: 'Cowboy Bebop',
+  artist: 'Seatbelts',
+  year: 1995
+})
+console.log(read('year',1995))
+
 
 // Routing functions go here
 app.get('/', (req, res) => {
-  res.render('index');
+  res.redirect('/tracks');
 });
 
+app.get('/tracks', (req, res) => {
+  res.render('index', {tracks: DB});
+});
+
+app.get('/tracks/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/tracks', (req, res) => {
+  console.log("New Track", req.body.name)
+  create({
+    number: req.body.number,
+    name: req.body.name,
+    artist: req.body.artist,
+    year: req.body.year
+  })
+  res.redirect('/tracks')
+})
+
+app.get('/tracks/:id', (req, res) => {
+  res.render('show',{track: read("number",req.params.id)});
+});
+
+app.put('/tracks/:id', (req, res) => {
+  console.log("I should edit ",req.params.id)
+  res.redirect('/tracks')
+});
+
+app.delete('/tracks/:id', (req, res) => {
+  
+});
 
 // Start the server
 const PORT = process.env.PORT || 8000;
