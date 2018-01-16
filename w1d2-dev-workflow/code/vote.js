1c0ff4e6d9f0cc1ac0fe146d2e72cc217e4861db
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-01-16 09:31:38 -0500

    Using an array to store genres

---
+++ w1d2-dev-workflow/code/vote.js
@@ -0,0 +1,10 @@
+// This is a new program
+// vote: figure out which music genre is most popular
+// among students today.
+
+// need some data to store answers to survey
+// how about an array
+
+var genres = ['hiphop','rockandroll','rockandroll','electronic','hiphop','80s','80s','electronic','80s','jazz','drumandbas','hiphop','randb','dance','hiphop','rap','rap','jazz','electronic']
+
+console.log(genres)
