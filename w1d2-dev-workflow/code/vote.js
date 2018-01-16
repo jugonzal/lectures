a91f180f4fcb97b162b57890fcefc92d89933ca9
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-01-16 09:49:45 -0500

    Started my loop

--- w1d2-dev-workflow/code/vote.js
+++ w1d2-dev-workflow/code/vote.js
@@ -7,4 +7,22 @@
 
 var genres = ['hiphop','rockandroll','rockandroll','electronic','hiphop','80s','80s','electronic','80s','jazz','drumandbas','hiphop','randb','dance','hiphop','rap','rap','jazz','electronic']
 
-console.log(genres)
+// console.log(genres)
+
+// we have two potential solutions
+// X sort and count  XXX
+// - loop and set a variable for each genre
+//  create an empty object to track counts
+
+var votes = {}
+//  start loop from beginning to end of array
+
+for (var g = 0; g < genres.length; g++) {
+  console.log(g, genres[g])
+  //  do I have a variable for this particular genre
+  //    count up this genre
+  //  if not
+  //    create that variable and set to 1
+  //    do this in a way that is flexible
+  
+}
