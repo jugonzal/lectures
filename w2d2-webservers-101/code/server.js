f1d3be857b45583333dc1b5d0a3e814ec9f99b7b
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-01-23 11:49:20 -0500

    Basic web server using express

--- w2d2-webservers-101/code/server.js
+++ w2d2-webservers-101/code/server.js
@@ -1,38 +1,50 @@
 const express = require('express');
 const app = express();
 
+var countVisitors = 0;
+
 app.use(function(req, res, next){
-    console.log(`New Request: ${req.method} ${req.url}`);
+    console.log(`Saw a new Request fly by... ${++countVisitors}`);
     next();
 });
 
-app.use(express.static('public'));
+app.use(function(req, res, next){
+    console.log(`Log Request: ${req.method} ${req.url}`);
+    next();
+});
 
 app.set("view engine", "ejs");
 
-
 app.get('/', function (req, res) {
-  res.send(`<h1>Welcome to our Fall Events.</h1>
-        <br />Go to <a href='/toronto'>Toronto</a> 
-        or <a href='/montreal'>Montreal</a> events`);
+  res.send(`
+        <html>
+          <body>
+            <h1>AWESOME Event Website</h1>
+            <p>Welcome to my awesome website</p>
+            <ul>
+              <li>Events in <a href="/toronto">Toronto</a></li>
+              <li>Events in <a href="/montreal">Montreal</a></li>
+            </ul>
+          </body>
+        </html>
+      `);
 });
 
 app.get('/toronto', function (req, res) {
-  res.render('toronto');
+  res.render('toronto')
 })
 
-app.get('/city/:name', function(req, res) {
-  res.send('No info found on ' + req.params.name);
-});
-
 app.get('/montreal', function(req, res) {
-  res.render('montreal');
+  res.render('montreal',{eventName: 'Awesome EVENTS' })
 })
 
 app.get('/calgary', function(req,res) {
-  res.status(500).send('Something broke!');
+  res.status(404).send('Under Construction!');
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+
