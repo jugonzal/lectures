d29440ff2a0a575dceaf755b214846a5f5b22b13
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-01-23 11:48:43 -0500

    Improved server with a few routes

--- w2d2-webservers-101/basic/server.js
+++ w2d2-webservers-101/basic/server.js
@@ -6,7 +6,52 @@ const PORT = 3000;
 let server = http.createServer(function whatToDoOnRequest (request, response){
     var fullRequest = request.method+' '+request.url;
     console.log("Request: ", fullRequest);
-    response.end("GOT IT!")
+
+    if (request.url == '/') {
+      response.end(`
+        <html>
+          <body>
+            <h1>AWESOME Event Website</h1>
+            <p>Welcome to my awesome website</p>
+            <ul>
+              <li>Events in <a href="/toronto">Toronto</a></li>
+              <li>Events in <a href="/montreal">Montreal</a></li>
+            </ul>
+          </body>
+        </html>
+      `)
+    } else if (request.url == '/toronto') {
+      response.end(`
+        <html>
+          <body>
+            <h1>AWESOME Event Website</h1>
+            <p>Events in Toronto</p>
+            <ul>
+              <li>TIFF</li>
+              <li>Christmas Market</li>
+            </ul>
+          </body>
+        </html>
+      `)
+    } else if (request.url == '/montreal') {
+      response.end(`
+        <html>
+          <body>
+            <h1>AWESOME Event Website</h1>
+            <p>Events in Montreal</p>
+            <ul>
+              <li>Jazz Festival</li>
+              <li>High fives for everyone</li>
+              <li>Igloofest</li>
+              <li>Nuit Blance</li>
+            </ul>
+          </body>
+        </html>
+      `)
+    } else {
+      response.statusCode = 404;
+      response.statusMessage = 'Not found';
+      response.end('under construction')    }
 });
 
 // start the server
