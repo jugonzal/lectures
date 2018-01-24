5962326cccd358b229f1497d39c76c4dd84d1504
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-01-24 11:11:45 -0500

    Updated after W2D3 January

--- w2d3-crud-with-express/initial/app.js
+++ w2d3-crud-with-express/initial/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const morgan = require('morgan');
 
@@ -9,95 +9,84 @@ const app = express();
 // Use EJS for views
 app.set("view engine", "ejs");
 
-// As we discussed before, Express is a very barebones library.
-// We can add different functionality using middlewares, which are functions
-// that process requests before they're handed to your routing functions.
-// See: http://expressjs.com/en/guide/using-middleware.html
-
-// To work with forms and JSON data, we need to configure Express
-// to use the bodyParser middleware to convert those types of data
-// into JS objects inside our functions.
 app.use(bodyParser.urlencoded({extended: false})); // forms
-// app.use(bodyParser.json()); // JSON
-
 app.use(methodOverride('_method'))
 
 // I'm also adding a logging middleware so we can see what's going on
 // with our server. More info: https://github.com/expressjs/morgan
-app.use(morgan('dev'));
+app.use(morgan('dev'));  // or 'combined' for a more detailed log
 
-let DB = [{ 
-  number: 1,
-  name: 'Mr Blue Sky',
-  artist: 'Electric Light Orchestra',
-  year: 1972
-}, {
-  number: 2,
-  name: 'Black Sabbath',
-  artist: 'Black Sabbath',
-  year: 1978  
+const DB = [{
+  title: "Greatest Hits",
+  artist: "Queen",
+  year: 1989
+},{
+  title: "Time Out",
+  artist: "Dave Brubeck",
+  year: 1959
 }]
 
-function create (track) {
-  DB.push(track)
-}
 
-function read (criteria, value) {
-  for (let i = 0; i< DB.length; i++) {
-    if (DB[i][criteria] == value) {
-      return DB[i]
-    } 
-  }
+function create (newLP) {
+  DB.push(newLP)
 }
 
 create({
-  number: 3,
-  name: 'Cowboy Bebop',
-  artist: 'Seatbelts',
-  year: 1995
+  title: "Kind of Blue",
+  artist: "Miles Davis",
+  year: 1966
 })
-console.log(read('year',1995))
 
+console.log(DB);
+
+function read (query, value) {
+  for (let lp of DB) {
+    if (lp[query] == value) {
+      //lp.year
+      //lp.artist
+      return lp;
+    }
+  }
+}
+
+console.log(read('year',1966))
 
 // Routing functions go here
 app.get('/', (req, res) => {
-  res.redirect('/tracks');
+  res.redirect('/lps');
 });
 
-app.get('/tracks', (req, res) => {
-  res.render('index', {tracks: DB});
-});
-
-app.get('/tracks/new', (req, res) => {
-  res.render('new');
-});
-
-app.post('/tracks', (req, res) => {
-  console.log("New Track", req.body.name)
-  create({
-    number: req.body.number,
-    name: req.body.name,
-    artist: req.body.artist,
-    year: req.body.year
-  })
-  res.redirect('/tracks')
+app.get('/create',(req, res) => {
+  res.render('create');
 })
 
-app.get('/tracks/:id', (req, res) => {
-  res.render('show',{track: read("number",req.params.id)});
+app.get('/lps', (req, res) => {
+  res.render('index',{database: DB});
+})
+
+app.post('/lps', (req, res) => {
+  console.log('Added LP for ',req.body.title)
+  create(req.body)
+  res.redirect('/lps')
+})
+
+app.get('/lps/:aYear', (req, res) => {
+  console.log('Found LP for ', read('year',req.params.aYear))
+  res.render('show',read('year',req.params.aYear))
+})
+
+app.put('/lps/:aYear', (req, res) => {
+  console.log("I should edit ",req.params.aYear," with ",req.body)
+  res.redirect('/lps')
 });
 
-app.put('/tracks/:id', (req, res) => {
-  console.log("I should edit ",req.params.id)
-  res.redirect('/tracks')
-});
-
-app.delete('/tracks/:id', (req, res) => {
-  
+app.delete('/lps/:aYear', (req, res) => {
+  console.log("Remind me to delete ",req.params.aYear)
+  res.redirect('/lps')
 });
 
 // Start the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
