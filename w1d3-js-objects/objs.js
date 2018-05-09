800036e50da769bca5cb993d1c0eb798d4233d8b
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-05-09 10:56:10 -0400

    W1D3 final version

--- w1d3-js-objects/objs.js
+++ w1d3-js-objects/objs.js
@@ -2,26 +2,41 @@
 
 'use strict';
 
-var nameOfCourse = 'Web Development Bootcamp';
+var course = {
+  nameOfCourse: 'Web Development Bootcamp',
+  startDate: 'May 9, 2018',
+  staff: ['sumi','saba']
+};
 
-var students = [];
-students.push( 'Sorin');
-students.push( 'Sara');
-students.push( 'Chantal');
+course.students = [];
+course.students.push( 'Sorin');
+course.students.push( 'Sara');
+course.students.push( 'Chantal');
 
-var teachers = [];
-teachers.push('Juan');
+course['staff'].push( 'Juan');
 
-var startDate = 'May 9, 2018';
+var newAttributeName = 'duration'
+course.duration = 10;
+course[newAttributeName] = 8;
 
-function changeDate (someDate, newDate) {
-  someDate = newDate;
+
+function changeDate (someCourse, newDate) {
+  someCourse.startDate = newDate;
   return;
 }
 
-changeDate(startDate, 'May 7, 2018')
+changeDate(course, 'May 7, 2018')
 
-console.log(nameOfCourse);
-console.log(startDate);
-console.log('Teachers: ',teachers);
-console.log('Students: ',students);
+var emptyCourse = {}
+changeDate(emptyCourse, 'June 2018')
+
+console.log(emptyCourse);
+
+// console.log(course.nameOfCourse);
+// console.log(course.startDate);
+// console.log('Teachers: ',course.teachers);
+// console.log('Students: ',course.students);
+
+console.log('Everything: ', course)
+
+console.log('One of our students is ', course.students[2]   )
