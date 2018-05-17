bebcb94819ca8c91c9fe10f628a6264fe55b1004
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-05-17 10:58:19 -0400

    W2D4 delivery for May2018 cohort

--- w2d4-cookies/language_selection/server.js
+++ w2d4-cookies/language_selection/server.js
@@ -2,43 +2,41 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000; // default port 8080
 
-// let languageSelected; // can't really have one global lang
+// const preferences = {
+//   language: 'none'
+// }
 
 app.set("view engine", "ejs");
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
 
-app.use(function (req, res, next) {
-  console.log("Cookies: ", req.cookies);
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser('this is juan gonzalez and nobody knows im el zorro'));
+
+app.use(function(req,res,next) {
+  console.log(req.cookies)
   next();
 })
 
 app.get("/", (req, res) => {
-  if (req.cookies.languageSelected == 'EN') {
-    res.render('english')
-  } else if (req.cookies.languageSelected == 'FR') {
-    res.render('french')
+  if (  req.cookies.language === 'english' ) {
+    res.render("english");  
+  } else if ( req.cookies.language === 'french' ) {
+    res.render("french");
   } else {
     res.render("choose_language");
   }
 });
 
 app.get("/choose_english", (req, res) => {
-  res.cookie ('languageSelected', 'EN');
+  // preferences.language = 'english';
+  res.cookie('language','english',{signed: true});
   res.render("english");
 })
 
 app.get("/choose_french", (req, res) => {
-  res.cookie ('languageSelected', 'FR');
+  // preferences.language = 'french';
+  res.cookie('language','french',{signed: true});
   res.render("french");
-});
-
-app.get("/information", (req, res) => {
-  if (req.cookies.languageSelected == 'EN') {
-    res.render("english")
-  } else {
-    res.render("french");
-  }
 });
 
 app.listen(PORT, () => {
