c2767914a44faebe977fd5bf0c01082df8d65b21
Juan Gonzalez <jugonzal@gmail.com>
2018-08-06 10:47:39 -0400

    updates to w2d1 after july cohort

--- w2d1-http-intro/code/weather.js
+++ w2d1-http-intro/code/weather.js
@@ -1,32 +1,18 @@
-// curl https://www.metaweather.com/api/location/4118/
-
 var request = require('request');
 
-// Get request
-request.get("https://www.metaweather.com/api/location/4118/", function(error, response, body) {
-  // If we have an error, we need to deal with it.
-  if (error) {
-    console.log("Boom! Deal with it!");
-    console.log(error);
-    return;
-  }
+request('https://www.metaweather.com/api/location/4118/', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+  //console.log('Response: ', response)
+  //console.log('body:', body); // Print the HTML for the Google homepage.
 
-  // console.log("*******************************\nResponse\n",response);
   if (response.statusCode === 200) {
-    // console.log(response);
-    // Once we JSON.parse the response, we have a regular JS object
-    //console.log("response-->", response);
-    if (response.headers['content-type'] == 'application/json' ) {
-      var data = JSON.parse(body);
-      console.log("Temperature today is :", data.consolidated_weather[0].the_temp);
-    } else {
-      console.log("Dont know how to handle ", response.headers['content-type'])
-    }
-    // console.log("Just the Postal Code:", data.results[0].address_components);
-    // console.log("Coordinates:",data.results[0].geometry.bounds.northeast.lat,data.results[0].geometry.bounds.northeast.lng);
+    var data = JSON.parse(body)
+    console.log("All of it: ", data)
+
+    console.log("Temperature: ",data.consolidated_weather[0].the_temp)
+    console.log("Humidity: ",data.consolidated_weather[0].humidity)    
+  } else {
+    console.log("Not found")
   }
-
 });
-
-
-
