9ef4698269920f399dfa496326a5edb7ee382593
Juan Gonzalez <jugonzal@gmail.com>
2018-11-12 11:00:03 -0500

    updates W4D1 after Oct2018 cohort

--- w4d1-sql-intro/queries.sql
+++ w4d1-sql-intro/queries.sql
@@ -1,4 +1,3 @@
-
 -- all tracks, with artist name and album name
 
 SELECT
@@ -18,18 +17,28 @@ SELECT
   albums.title AS album,
   artists.name AS artist
 FROM tracks, albums, artists
-WHERE albums.artist_id = artists.id AND tracks.album_id = albums.id;
+WHERE albums.artist_id = artists.id 
+AND tracks.album_id = albums.id;
 
 
 -- all artists with tags
 
 SELECT
-  a.name AS artist,
-  string_agg(t.name,', ')
-FROM artists_tags AS at
-JOIN artists AS a ON at.artist_id = a.id 
-JOIN tags AS t ON at.tag_id = t.id
+  artists.name AS artist, tags.*
+FROM artists, tags, artists_tags as at 
+WHERE at.artist_id = artists.id 
+AND at.tag_id = tags.id
+
+SELECT
+  artists.name AS artist,
+  string_agg(tags.name, ', ') AS tag,
+  count(*)
+FROM artists, tags, artists_tags as at 
+WHERE at.artist_id = artists.id 
+AND at.tag_id = tags.id
 GROUP BY artist;
+
+
 
 
 -- # of albums per artist
@@ -46,13 +55,19 @@ GROUP BY artists.id;
 -- who have more than 2 albums, in descending order
 
 SELECT
-  artists.name as artist,
-  COUNT(albums.id) as album_count
+  artists.name, count(artists.id)
 FROM artists
 JOIN albums ON albums.artist_id = artists.id
 GROUP BY artists.id
-HAVING COUNT(albums.id) >= 2
-ORDER BY album_count DESC;
+having count(artists.id) > 1
+
+SELECT
+  artists.name, count(artists.id)
+FROM artists
+JOIN albums ON albums.artist_id = artists.id
+WHERE albums.year > 2000
+GROUP BY artists.id
+having count(artists.id) > 1;
 
 
 -- average tracks per album
@@ -71,16 +86,15 @@ WHERE tracks.album_id = albums.id;
 
 -- average number of tracks in albums of each tag, in order
 
-  SELECT
-    tags.name as tag,
-    COUNT(tracks.id) / COUNT(distinct albums.id) AS avg
-  FROM tags, artists, artists_tags, albums, tracks
-  WHERE artists_tags.artist_id = artists.id
-  AND artists_tags.tag_id = tags.id
-  AND albums.artist_id = artists.id
-  AND albums.id = tracks.album_id
-  GROUP BY tags.id
-  ORDER BY avg DESC;
+SELECT
+  tags.name as tag,
+  COUNT(tracks.id) / COUNT(distinct albums.id) AS avg
+FROM tags, artists, artists_tags AS at, albums, tracks
+WHERE at.artist_id = artists.id AND at.tag_id = tags.id
+AND albums.artist_id = artists.id
+AND albums.id = tracks.album_id
+GROUP BY tag
+ORDER BY avg DESC;
 
 
 -- demonstrate the usefulness of an outer join when we don't know where data will exist
