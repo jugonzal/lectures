0b1e0be0a8511807170ffb3817ed66700d272370
Juan Gonzalez <jugonzal@gmail.com>
2018-12-03 11:02:57 -0500

    Updated code for W2D1 after nov2018 cohort

--- w2d1-http-intro/code/weather.js
+++ w2d1-http-intro/code/weather.js
@@ -2,17 +2,20 @@ var request = require('request');
 
 request('https://www.metaweather.com/api/location/4118/', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
-  //console.log('Response: ', response)
-  //console.log('body:', body); // Print the HTML for the Google homepage.
+  if (!error) {
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    if (response.headers['content-type'].includes('json')) {
+      console.log('Content Type: ', response.headers['content-type'])
 
-  if (response.statusCode === 200) {
-    var data = JSON.parse(body)
-    console.log("All of it: ", data)
+      // console.log('My lame attempt to find temp: ',body.substring(70,10))
 
-    console.log("Temperature: ",data.consolidated_weather[0].the_temp)
-    console.log("Humidity: ",data.consolidated_weather[0].humidity)    
-  } else {
-    console.log("Not found")
+      var weather = JSON.parse(body)
+      // console.log('Full Body: ',weather)
+      console.log('Current Temp: ', weather.consolidated_weather[0].the_temp)
+    } else {
+      console.log('Big web page... you dont want to read')
+    }
   }
 });
+
+console.log('Once the request is sent...')
