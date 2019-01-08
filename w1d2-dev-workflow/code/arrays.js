40f0cbd1a7fdf1fd85fb33677ed86f8093d364ec
Juan Gonzalez <jugonzal@gmail.com>
2019-01-08 09:44:40 -0500

    Define a new problem to add up freq

---
+++ w1d2-dev-workflow/code/arrays.js
@@ -0,0 +1,20 @@
+
+// Talk about some big problem
+// read specs / requirements
+// try to come up with a solution... together
+
+// a function that takes arrays
+// make sure it is an array
+  // Array.isArray(myArray)
+//  what if it is not ?  convert ?
+  // maybe it is a long string
+  // str.split(', ')
+  // maybe it is a file
+// dont assume the initial freq is zero... it could be some other number
+// iterate through all elements of the array
+// calculates the sum of all changes/freq
+
+// code the solution
+
+
+// test the solution
