4d1d116b8779868fc9714cd64666b13982b8650d
jugonzal <jugonzal@gmail.com>
2019-03-26 08:34:46 -0400

    changed the sequence of the breakout

--- w4d1bo-more-sql/queries.sql
+++ w4d1bo-more-sql/queries.sql
@@ -24,7 +24,75 @@ WHERE albums.artist_id = artists.id AND tracks.album_id = albums.id;
 SELECT * 
 FROM albums, artists
 WHERE albums.artist_id = artists.id
-  AND albums.year < 2005;
+  AND albums.year < 2000;
+
+-- sometimes you need to step back and see the data
+-- as a whole
+
+SELECT year, count(*)
+FROM albums
+GROUP BY year;
+
+-- # of albums per artist
+
+SELECT
+  artists.name,
+  COUNT(albums.id) as album_count
+FROM artists
+JOIN albums ON albums.artist_id = artists.id
+GROUP BY artists.name;
+
+
+-- # of albums per artist
+-- who have more than 2 albums, in descending order
+
+SELECT
+  artists.name as artist,
+  COUNT(albums.id) as album_count
+FROM artists
+JOIN albums ON albums.artist_id = artists.id
+GROUP BY artist
+HAVING COUNT(albums.id) >= 2
+ORDER BY album_count DESC;
+
+
+SELECT
+  artists.name as artist,
+  string_agg(albums.title, ', ')
+FROM artists
+JOIN albums ON albums.artist_id = artists.id
+GROUP BY artist
+HAVING COUNT(albums.id) >= 2;
+ORDER BY album_count DESC;
+
+
+
+-- average tracks per album
+
+SELECT COUNT(distinct tracks.id) / COUNT(distinct albums.id) AS average
+FROM albums, tracks;
+
+
+-- same as above
+-- but using an implicit JOIN instead of DISTINCT
+
+SELECT COUNT(tracks.id) / COUNT(distinct albums.id) AS avg
+FROM albums, tracks
+WHERE tracks.album_id = albums.id;
+
+
+-- average number of tracks in albums of each tag, in order
+
+SELECT
+  tags.name as tag,
+  COUNT(tracks.id) / COUNT(distinct albums.id) AS avg
+FROM tags, artists, artists_tags AS at, albums, tracks
+WHERE at.artist_id = artists.id AND at.tag_id = tags.id
+AND albums.artist_id = artists.id
+AND albums.id = tracks.album_id
+GROUP BY tag
+ORDER BY avg DESC;
+
 
 -- sometimes joins get more than 2 tables involved.   
 -- This was a good opportunity to talk about many-to-many
@@ -64,7 +132,7 @@ FROM tags, artists, artists_tags
 WHERE artists_tags.artist_id = artists.id
   AND artists_tags.tag_id = tags.id 
   AND tags.name in (
-    select name from tags where id > 2) 
+    select name from tags where id < 4) 
 GROUP BY artists.name 
 HAVING count(*) > 1;
 
@@ -75,7 +143,7 @@ SELECT
 FROM tags, artists, artists_tags
 WHERE artists_tags.artist_id = artists.id
   AND artists_tags.tag_id = tags.id 
-  AND tags.name in (select name from tags where id > 2) 
+  AND tags.name in (select name from tags where id < 4) 
 GROUP BY artists.name 
 HAVING count(*) > 1;
 
@@ -104,56 +172,6 @@ WHERE artists_tags.artist_id = artists.id
   )
 GROUP BY artists.name;
 
--- # of albums per artist
-
-SELECT
-  artists.name as artist,
-  COUNT(albums.id) as album_count
-FROM artists
-JOIN albums ON albums.artist_id = artists.id
-GROUP BY artists.id;
-
-
--- # of albums per artist
--- who have more than 2 albums, in descending order
-
-SELECT
-  artists.name as artist,
-  COUNT(albums.id) as album_count
-FROM artists
-JOIN albums ON albums.artist_id = artists.id
-GROUP BY artist
-HAVING COUNT(albums.id) >= 2
-ORDER BY album_count DESC;
-
-
--- average tracks per album
-
-SELECT COUNT(distinct tracks.id) / COUNT(distinct albums.id) AS average
-FROM albums, tracks;
-
-
--- same as above
--- but using an implicit JOIN instead of DISTINCT
-
-SELECT COUNT(tracks.id) / COUNT(distinct albums.id) AS avg
-FROM albums, tracks
-WHERE tracks.album_id = albums.id;
-
-
--- average number of tracks in albums of each tag, in order
-
-SELECT
-  tags.name as tag,
-  COUNT(tracks.id) / COUNT(distinct albums.id) AS avg
-FROM tags, artists, artists_tags AS at, albums, tracks
-WHERE at.artist_id = artists.id AND at.tag_id = tags.id
-AND albums.artist_id = artists.id
-AND albums.id = tracks.album_id
-GROUP BY tag
-ORDER BY avg DESC;
-
-
 -- demonstrate the usefulness of an outer join 
 -- when we don't know where data will exist
 
@@ -163,6 +181,12 @@ left outer join plays on plays.track_id = tracks.id
 inner join albums on tracks.album_id = albums.id
 group by albums.title;
 
+
+select albums.title, sum(count) 
+from tracks
+join plays on plays.track_id = tracks.id
+join albums on tracks.album_id = albums.id
+group by albums.title;
 
 
 
