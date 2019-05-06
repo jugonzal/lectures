e19e3bb044e4804ae686c853a953e13273c5ecf9
jugonzal <jugonzal@gmail.com>
2019-05-06 11:17:39 -0400

    updates to W2D1 after May2019 cohort

--- w2d1-http-intro/code/albums.js
+++ w2d1-http-intro/code/albums.js
@@ -4,21 +4,27 @@ var request = require('request');
 // Some URLs:
 // 	http://jsonplaceholder.typicode.com/albums?userId=8
 //  https://techmeme.com/img/techmeme135.png
-request.get('http://jsonplaceholder.typicode.com/albums?userId=8', function (error, response, body) {
-	// console.log('Got it!');
-	if (error) {
-		console.log("There was an ERROR: ", error);
-	} else if (response.statusCode == 200) {
-		// console.log("Body ---------------- ")
-		console.log(body)
-	} else {
-		console.log("Weird Status: ", response.statusCode)
-	}
+request('http://jsonplaceholder.typicode.com/albums?userId=8', function (error, response, body) {
+  if (error) {
+	console.log('error:', error); // Print the error if one occurred
+	return;
+  } else if (response && response.statusCode === 200) {
+	  console.log('body:', body); // Print the HTML for the Google homepage.
+	  var data = JSON.parse(body);
+	  // console.log('data:', data);
+	  data.forEach(function(user) {
+	  	console.log(user.title);
+	  })
+  } else {
+  	console.log("Some other status code: ",  response.statusCode)
+  }
 });
+
+
 // the following line was used to prove that the 
 // response could come AFTER the program appeared 
 // to end.
-// console.log("All Done.  Out of here.")
+console.log("All Done.  Out of here.")
 
 
 
