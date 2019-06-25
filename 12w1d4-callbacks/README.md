590a9e4d4f801ad09bf654c81d1e15375dccffd2
jugonzal <jugonzal@gmail.com>
2019-06-25 15:04:41 -0400

    draft of W1D4 lecture for 12W program

---
+++ 12w1d4-callbacks/README.md
@@ -0,0 +1,16 @@
+Hi all, this morning we had a really good session writing good, useful functions.
+In particular functions that helped me manage my Tweeps.  I like that we explored
+many different challenges and solved them all with functions, always trying to 
+write better code.
+
+A few things we talked about:
+
+- Functions can be created by _declaration_ or _expression_.
+- In *javascript*, functions are "first class citizens". For example we can put them in variables.
+- Variables declared within a function only exist within that function. Be mindful of functions that use variables without declaring as they may be stepping on existing variables unintentionally.
+- A function should do ONE thing and one thing alone.  This is why we decomposed many little actions into their own little functions.
+- Perhaps the most important trick we learned was that a function can be passed as a parameter to another function. This is what we call a "callback"
+- Overall we were looking for _better_ ways to write functions and what we discovered is that _general_ functions that do one thing really well are much more preferable. You'll soon read about *High Order* functions and discover a world of such functions.
+
+You can find all of the code we wrote in [my github repo](https://github.com/jugonzal/lectures/blob/master/12w1d4-callbacks/tweeps.js)
+
---
+++ 12w1d4-callbacks/tweeps.js
@@ -0,0 +1,118 @@
+const tweeps = [
+  { who: '@fzero', 
+    said: "Callbacks are control flow as designed by M.C. Escher."},
+  { who: '@kevinroose',
+    said: "The solution to Bad Facebook is always More Facebook"},
+  { who: '@0xUID',
+    said: "Macintosh OS X was beautiful, intuitive and user-friendly......and it was #UNIX!"},
+  { who: '@mozilla',
+    said: "Today, Mozilla is publishing the 2019 #InternetHealth Report"}
+]
+
+  // STAGE 0: Our first stage is to 
+  // manually change one tweep at a time
+
+// tweeps[0].when = new Date("May 1, 2019");
+// tweeps[0].when = tweeps[0].when.toString();
+
+// tweeps[3].when = (new Date("04/23/2019")).toString();
+
+// tweeps[1].liked = true;
+// tweeps[2]['liked'] = true;
+// var myKey = 'liked';
+// // tweeps[3].myKey = true;   <-- this will not work!
+// tweeps[3][myKey] = true;
+
+
+
+  // STAGE 1: writing functions to do one thing
+  // Explore the various styles of functions: 
+  // declaration vs expression
+  // Also, what happens to variables that go into functions?
+
+function likeTweep (nameOfTweep) {
+  for (i=0; i< tweeps.length; i++) {
+    if (tweeps[i].who === nameOfTweep) {
+      tweeps[i].like = true;
+    }
+  }
+}
+
+// function timestamp () {
+//   for (i=0; i< tweeps.length; i++) {
+//     tweeps[i].when = (new Date()).toString();
+//   }
+// }
+
+
+// console.log(typeof likeTweep);
+
+// var reallyLikeTweep = likeTweep;
+
+// reallyLikeTweep('@fzero');
+// timestamp();
+
+
+  // STAGE 2: finding ways to create generic functions, 
+  // using bracket notation
+
+// function changeTweep (nameOfTweep, doWhat, value) {
+//   for (i=0; i< tweeps.length; i++) {
+//     if (tweeps[i].who === nameOfTweep) {
+//       tweeps[i][doWhat] = value;
+//     }
+//   }
+// }
+
+// changeTweep('@fzero','like', true);
+// changeTweep('@fzero','like', false);
+// changeTweep('@mozilla','useful', 'yes');
+// changeTweep('@fzero','when',(new Date()))
+
+
+
+  // STAGE 3: certain things that our function above 
+  // can not do, such as changing what was said. 
+
+// function changeTweep (nameOfTweep, doSomething) {
+//   for (i=0; i< tweeps.length; i++) {
+//     if (tweeps[i].who === nameOfTweep) {
+//       console.log(doSomething, tweeps[i])
+//       doSomething(tweeps[i])
+//     }
+//   }
+// }
+
+// function likeTweep(tweep) {
+//   // console.log('likeTweep', tweep)
+//   tweep.like = true;
+// }
+
+// changeTweep('@fzero',likeTweep);
+
+// changeTweep('@mozilla',function(tweep) {
+//   tweep.when = (new Date());
+// })
+
+// changeTweep('@kevinroose', function(tweep) {
+//   tweep.said = tweep.said.toUpperCase();
+// })
+
+// console.log(tweeps);
+
+// changeTweep('@fzero','like', false);
+// changeTweep('@fzero','when',(new Date()))
+
+
+  // I've created a function that does ONE thing very
+  // well, but at the same time is so generic that could
+  // be used to make ANY change across all my tweeps.
+  // That is the ultimate goal.
+
+// Javascript provides a range of high order function like `map`
+//
+// tweeps.map(function(tweep) {
+//   tweep.when = (new Date())
+// })
+
+console.log(tweeps);
