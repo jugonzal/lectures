998c5a89440d9aacfe2de4d73289dff749a5652e
jugonzal <jugonzal@gmail.com>
2019-06-25 13:09:59 -0400

    updates to W1D2 lecture for Jul2019 cohort

--- 12w1d2-dev-workflow/arrays.js
+++ 12w1d2-dev-workflow/arrays.js
@@ -1,23 +1,33 @@
-// Understanding a problem 
-// Generalize vs specialize
-// Experiment with REPL
-// Write first version
-// More advanced debugging techniques
-// Refactor our code
-// Use/Reuse functions
-// Big test !
+// collect a bunch of numbers
+// add them up
+// display final result
 
+// in case I want to get data from command line
+// let input = process.argv.slice(2);
 
-let array = process.argv.slice(2);
+let addThemUp = function(input) {
+  let total = 0;
 
-var addThemUp = 0;
-for (i=0; i< array.length; i++) {
-	addThemUp += Number(array[i]);
-	console.log("Working... ", i, addThemUp);
-}
+  for (let i = 0; i < input.length; i++) {
+    // console.log("i: ",i, parseInt(input[i]));
+    total = total + parseInt(input[i]);
+    // console.log("total: ", total);
+  }
+  return total;
+};
 
-console.log("Answer: ", addThemUp);
+console.log("First case: ", addThemUp(['+1','-3','+2','+1']));
+console.log("Second case: ", addThemUp(['+1', '+1', '+1']));
+console.log("Third case: ", addThemUp(['-1', '-2', '-3']));
 
+let freq = `-19
++2
++18
++14
+-3
+-16
++18
+-16`
 
-
+console.log("Final test: ", addThemUp(freq.split('\n')));
 
