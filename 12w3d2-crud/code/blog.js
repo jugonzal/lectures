928b3ef30031436796b2ac58898f10ced977f3a6
jugonzal <jugonzal@gmail.com>
2019-10-01 12:18:51 -0400

    updates to W3D2 after Sep 2019 cohort

--- 12w3d2-crud/code/blog.js
+++ 12w3d2-crud/code/blog.js
@@ -5,37 +5,44 @@ const methodOverride = require('method-override');
 const PORT = 8000;
 
 app.set("view engine", "ejs");
-// app.use(express.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(methodOverride('_method'));
+
 
 app.get('/', function(req, res) {
-  res.redirect('/articles')
+  res.redirect('/posts')
 })
 
-app.get('/articles', function (req, res) {
-  res.render('index', { posts: db.all() })
+app.get('/posts', function (req, res) {
+  res.render('index', {posts: db.all()})
 });
 
-app.get('/articles/:postId', function (req, res) {
-  res.render('one', { post: db.all()[req.params.postId] })
+app.get('/posts/:id', function(req, res) {
+  if (req.params.id < db.count()) {
+    res.render('one', { post: db.one(req.params.id) })
+  } else {
+    res.status(404).send("No no...  you probably meant <a href='/posts'> the list of posts</a>")
+  }
+  // res.send('Post: ' + db.one(req.params.id).body)
+})
+
+app.put('/posts/:id', function(req, res) {
+  db.update(req.params.id, req.body)
+  res.redirect('/posts/'+req.params.id)
+})
+
+app.post('/posts', function(req, res) {
+  // console.log('someone posted a post ', req.body)
+  db.add(req.body)
+  res.redirect('/posts')
 })
 
 app.get('/new', function(req, res) {
   res.render('new')
 })
 
-app.post('/articles', express.urlencoded({extended: false}), function(req, res) {
-  db.add(req.body)
-  res.redirect('/')
-})
-
-app.get('/edit/:postId', function(req, res) {
-  res.render('edit', { post: db.one(req.params.postId) })
-})
-
-app.put('/articles/:postId',express.urlencoded({extended: false}), function(req, res) {
-  db.update(req.params.postId, req.body)
-  res.redirect('/articles/'+req.params.postId)
+app.get('/edit/:id', function(req, res) {
+  res.render('edit', { post: db.one(req.params.id) })
 })
 
 app.listen(PORT, function () {
